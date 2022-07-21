2239fc30af458cff95f8547cf64c29dfbbfd4898
Christina Harris <harrischristina4688@gmail.com>
2022-07-20 20:12:11 -0400

    changed made to taskmanager file

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,185 +1,304 @@
-const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
-    const html = `
-      <div class="card-list" data-task-id="${id}">
-      <div class="card card-shadow p-2 mb-3">
-
-      <div class="card-body">
-        <!-- Task name --------------->
-        <div class="row mb-3 align-items-start justify-content-between">
-          <span class="col-7">
-            <h5 class="card-subtitle card-heading-primary mb-1">Task: </h5>
-            <p class="text-secondary">${name}</p>
-          </span>
-          
-          <span class="col-4 badge pb-1 text-capitalize me-3 ${
-            status === 'to-do'
-              ? 'bg-success'
-              : status === 'review'
-              ? 'bg-danger'
-              : status === 'in progress'
-              ? 'bg-warning text-dark'
-              : status === 'done'
-              ? ' bg-secondary'
-              : ''
-          }
-          ">${status}</span>
-        </div>
-        <!-- Assigned to -------------->
-        <div class="row mb-3">
-          <div class="col">
-            <h6 class="card-subtitle card-heading-primary mb-1 ">Assigned to: </h6>
-            <p class="text-secondary">${assignedTo}</p>
-          </div>
-
-          </div>
-        <!-- Description -------------->
-        <div class="row mb-3">
-          <h6 class="card-subtitle card-heading-primary mb-1 ">Description: </h6>
-          <p class="card-text text-secondary">
-            ${description}
-          </p>
-        </div>
-        <!-- Date, edit and delete buttons ---->
-        <div class="row align-items-center">
-          <h6 class="card-subtitle card-heading-primary mb-1 ">Due date: </h6>
-          <p class="card-text text-primary">
-          ${dueDate}
-          </p>
-
-          <div class="col d-flex justify-content-end">
-           ${
-             status === 'done'
-               ? ''
-               : '<span class="d-flex align-items-center" id="done-container"><button type="button" class="btn btn-outline-secondary btn-sm done-button">Done</button></span>'
-           }
-            <span class="d-flex justify-content-center "><i class="bi bi-x-lg icon-btn delete-button"></i></span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-    `;
+// Create Task Function
+function createTask(title, description, assignedTo, dueDate, status, id) {
+    const html = 
+    `<div class="card" data-task-id=${id}>
+        <img src="images/edit-icon.svg" alt="Edit button" class="edit-icon">
+            <div class="card-container">
+                <h1>${title}</h1>
+                <p class="card-desc">${description}</p>
+                <p class="card-status status-${status}">${status.toUpperCase()}</p>
+                <span class="button-alt-text button-alt-text-id${id}"></span>
+                <input type="image" src="images/checkmark.png" alt="Done Button" class="done-button button-clicked-${status}">
+                <input type="image" src="images/delete.svg" alt="Delete Button" class="delete-button">
+            </div>
+            <div class="card-footer footer-${status}">
+                <span class="card-due-date">${dueDate}</span>
+                <span class="card-assigned">${assignedTo}</span>
+            </div>
+    </div>`;
     return html;
-        };
-class taskManager {
+};
 
-    // Properties
+
+
+// Define TaskManager Class
+class TaskManager {
     constructor(currentId = 0) {
-        this.task = [];
+        this.tasks = [];
         this.currentId = currentId;
+    }; 
+    
+    // Method for creating a new task
+    addTask(title, description, assignedTo, dueDate, status) {
+        const task = {
+            // Increment the current Id for each new task
+            id: this.currentId++,
+            title: title,
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            status: status,
+          };
+      
+          this.tasks.push(task);
+    } 
+
+    updateTask(title, description, assignedTo, dueDate, status, taskId, taskPosition) {
+        const task = {
+            id: taskId,
+            title: title,
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            status: status,
+          };
+      
+          this.tasks[taskPosition] = task;
     }
 
-    // Methods
-  // Add task method
-  addTask(name, description, assignedTo, dueDate, status) {
-    // increment id
-    let id = this.currentId++;
+    // Method to delete task
+    deleteTask(taskId) {
+        let result = window.confirm("Are you sure you want to delete this task?")
+        if (result) {
+            const newTasks = [];
+            for(let i=0; i<this.tasks.length; i++) {
+                const task = this.tasks[i];
+                if (task.id !== taskId) {
+                    newTasks.push(task)
+                } 
+            }
+            this.tasks = newTasks;
+        clearForm();
+        this.render();
+        this.save();
+        }
+    }   
 
-    // push to tasks array
-    this.tasks.push({
-      id,
-      name,
-      description,
-      assignedTo,
-      dueDate,
-      status,
-    });
-  }
+    // Render task array into HTML display
+    render() {
+        let tasksHtmlList = [];
+        for(let i = 0; i < this.tasks.length;i++) {
+            
+            let formattedValues = shortenCardValues(
+                this.tasks[i].title,
+                this.tasks[i].description,
+                this.tasks[i].assignedTo,
+                this.tasks[i].dueDate,
+                this.tasks[i].status,
+                this.tasks[i].id);
+            
+            
+            // Create a HTML to represent the information retrieved and formatted above. Then push to a single array so that all HTML elements can be joined and rendered later.
+            const taskHtml = createTask(formattedValues.title, formattedValues.description, formattedValues.assigned, formattedValues.date, formattedValues.status, formattedValues.id);
+            tasksHtmlList.push(taskHtml);
 
-  // Render Method
-  render() {
-    // declare status arrays to categorise each task
-    let doneHtmlList = [];
-    let reviewHtmlList = [];
-    let todoHtmlList = [];
-    let inprogressHtmlList = [];
+            // Set current menu filter to display "Show All"
+            const currentFilter = document.getElementById("current-filter");
+            currentFilter.innerHTML = "Show All";
+        }
 
+        const tasksHtml = tasksHtmlList.join("\n");
+        const taskCardList = document.getElementById("task-card-list");
+        taskCardList.innerHTML = tasksHtml;
 
-    this.tasks.forEach(task => {
-      // Date objects contain a Number that represents milliseconds since 1 January 1970 UTC e.g. Sun Dec 17 1995 03:24:00 GMT...
-      let date = new Date(task.dueDate);
-      // format date to readable string e.g. 01/07/2021
-      let formattedDate = date.toLocaleDateString();
-      // pass values into create html card function
-      let taskHtml = createTaskHtml(
-        task.id,
-        task.name,
-        task.description,
-        task.assignedTo,
-        formattedDate,
-        task.status
-      );
-
-      // Push cards into approriate status array
-      if (task.status === 'review') {
-        reviewHtmlList.push(taskHtml);
-      } else if (task.status === 'to-do') {
-        todoHtmlList.push(taskHtml);
-      } else if (task.status === 'in progress') {
-        inprogressHtmlList.push(taskHtml);
-      } else {
-        doneHtmlList.push(taskHtml);
-      }
-    });
-
-    // Join cards and insert them into the approriate div tag
-    let reviewHtml = reviewHtmlList.join('\n');
-    document.getElementById('review').innerHTML = reviewHtml;
-    let todoHtml = todoHtmlList.join('\n');
-    document.getElementById('todo').innerHTML = todoHtml;
-    let inprogressHtml = inprogressHtmlList.join('\n');
-    document.getElementById('inprogress').innerHTML = inprogressHtml;
-    let doneHtml = doneHtmlList.join('\n');
-    document.getElementById('done').innerHTML = doneHtml;
-  }
-
-  // Save Method
-  save() {
-    // Create a string for all tasks
-    let tasksJson = JSON.stringify(this.tasks);
-    // Store the string variable in local storage under key 'tasks'
-    localStorage.setItem('tasks', tasksJson);
-    // convert currentId to stirng
-    let currentId = JSON.stringify(this.currentId);
-    // Store the string variable in local storage under key 'currentId'
-    localStorage.setItem('currentId', currentId);
-  }
-
-  // Load Method
-  load() {
-    // check if any tasks are saved or exists in localStorage, error will produce if its empty
-    if (localStorage.getItem('tasks') !== null) {
-      const tasksJson = localStorage.getItem('tasks');
-      // Convert the tasksJson string to an array and store it in this.tasks
-      this.tasks = JSON.parse(tasksJson);
+        if(this.tasks.length === 0) {
+            taskCardList.innerHTML = `<div class="card" data-task-id="">
+            <img src="images/edit-icon.svg" alt="Edit button" class="edit-icon">
+            <div class="card-container">
+              <h1>Welcome</h1>
+              <p class="card-desc">Press the burger icon on the left to create a new form :)</p>
+              <p class="card-status">STATUS</p>
+            </div>
+            <div class="card-footer">
+              <span class="card-due-date">Due Date</span><span class="card-assigned">Assigned To</span>
+            </div>   
+          </div>`;
+        }
     }
-    // check if the currentId is saved in localStorage
-    if (localStorage.getItem('currentId') !== null) {
-      const currentId = localStorage.getItem('currentId');
-      //Convert the currentId to a number before storing in this.currentId
-      this.currentId = parseInt(currentId);
+
+    // Method to filter by status
+    renderByFilter(className) {
+        let tasksHtmlList = [];
+        for(let i = 0; i < this.tasks.length;i++) {
+            if(this.tasks[i].status===className) {
+                let formattedValues = shortenCardValues(
+                    this.tasks[i].title,
+                    this.tasks[i].description,
+                    this.tasks[i].assignedTo,
+                    this.tasks[i].dueDate,
+                    this.tasks[i].status,
+                    this.tasks[i].id);
+                
+                
+                // Create a HTML to represent the information retrieved and formatted above. Then push to a single array so that all HTML elements can be joined and rendered later.
+                const taskHtml = createTask(formattedValues.title, formattedValues.description, formattedValues.assigned, formattedValues.date, formattedValues.status, formattedValues.id)
+                tasksHtmlList.push(taskHtml);
+            }
+
+        }
+        const tasksHtml = tasksHtmlList.join("\n");
+        const taskCardList = document.getElementById("task-card-list");
+        taskCardList.innerHTML = tasksHtml;
+
+        // Set current menu filter to display correct value
+        const currentFilter = document.getElementById("current-filter");
+        currentFilter.innerHTML = className;
+
     }
-  }
+    
+    // Finds Task by matching card info with stored array of tasks. Returns the task object found
+    getTaskId(taskId) {
+        let foundTask;
+        
+        // Compares the taskId (button task id) to the looped task
+        this.tasks.forEach((getTask) => {
+            const task = getTask;
+            if(task.id === taskId) {
+            foundTask = task;
+        }
+        });
+        return foundTask;
+    }
 
-  // Get task by the id
-  getTaskById(taskId) {
-    let foundTask;
-    this.tasks.find(task => {
-      if (task.id === taskId) {
-        foundTask = task;
-      }
-    });
-    return foundTask;
-  }
+    // Finds Task by matching card info with stored array of tasks. Returns the index (i.e. position) of the card in the array
+    getTaskPosition(taskId) {
+        let foundIndex = 0;
+        
+        // Loops through task array
+        for (let i=0; i<this.tasks.length; i++) {
+            const task = this.tasks[i]
+            // When task selected is the same as array, return index number
+            if(task.id === taskId) {
+                foundIndex = i;
+            }
+        }
 
-  // Delete task by id
-  deleteTask(taskId) {
-    let newTasks = [];
-    this.tasks.forEach(task => {
-      if (task.id !== taskId) {
-        newTasks.push(task);
-      }
-    });
-    this.tasks = newTasks;
-  }
+        return foundIndex;
+    }
+
+    // Save to Local Storage
+    save() {
+        // Save tasks array
+        let tasksJson = JSON.stringify(this.tasks);
+        localStorage.setItem("tasks", tasksJson);
+
+        // Save current position/value of currentId
+        let currentId = this.currentId;
+        localStorage.setItem("currentId", currentId);
+    }
+
+    // Load from Local Storage
+    load() {
+        // Check if any tasks are locally stored, then retrieve/assign value
+        if (localStorage.getItem("tasks")) {
+            let tasksJson = localStorage.getItem("tasks");
+            this.tasks = JSON.parse(tasksJson);
+            taskManager.render();
+        } else {
+            document.getElementById("task-card-list").innerHTML = `<div class="card" data-task-id="">
+            <img src="images/edit-icon.svg" alt="Edit button" class="edit-icon">
+            <div class="card-container">
+              <h1>Welcome</h1>
+              <p class="card-desc">Press the burger icon on the left to create a new form :)</p>
+              <p class="card-status">STATUS</p>
+            </div>
+            <div class="card-footer">
+              <span class="card-due-date">Due Date</span><span class="card-assigned">Assigned To</span>
+            </div>   
+          </div>`
+        };
+
+        // Check if currentId is stored, then retrieve/assign value
+        if (localStorage.getItem("currentId")) {
+            let currentId = localStorage.getItem("currentId");
+            this.currentId = parseInt(currentId);
+            taskManager.render();
+        } else {
+            this.currentId = 0;
+        }
+    }
+
+
+
+
+}     
+
+
+// Shorten values to render neatly
+function shortenCardValues (title, description, assigned, dueDate, status, id) {
+    // Assign HTML to display when value is formatted and shortened
+    const extraWords = "<span style='color: lightgrey'> ...</span>"
+
+    // Format each task before rendering (e.g. title is too long)
+    let formattedTitle = ""; 
+        if(title.length > 30) {
+            formattedTitle = title.substring(0,30) + extraWords;
+        } else {
+            formattedTitle = title;
+        }
+
+    let formattedDescription = ""; 
+        if(description.length > 300) {
+            formattedDescription = description.substring(0,300) + extraWords;
+        } else {
+            formattedDescription = description;
+        } 
+
+    let formattedAssigned = ""; 
+        if(assigned.length > 20) {
+            formattedAssigned = assigned.substring(0,20) + extraWords;
+        } else {
+            formattedAssigned = assigned;
+        }
+    
+    const date = new Date(dueDate);
+    const formattedDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+    
+    let formattedValues = {
+        title: formattedTitle, 
+        description: formattedDescription, 
+        assigned: formattedAssigned, 
+        date: formattedDate, 
+        status: status, 
+        id: id};
+
+    return formattedValues;
 }
+
+// Submit Form Functions (validates inputs -> runs .addTask method)
+function submitForm (newTask) {
+    // Only run if input is valid
+    if (validateInput()) {
+        
+        // Add New Task
+        if(newTask === "new") {
+            taskManager.addTask(formTitle.value, formDescription.value, formAssignedTo.value, formDue.value, formStatus.value);
+            window.alert(`Task created successfully! :)`);   
+        }
+
+        // Update Existing Task
+        if(newTask === "update") {
+            taskManager.updateTask(formTitle.value, formDescription.value, formAssignedTo.value, formDue.value, formStatus.value, updateTaskId, updateTaskPosition);
+            window.alert(`Task updated successfully! :)`);
+        }
+        
+        // Reset form to default, update tasks displayed, save to local storage
+        clearForm();
+        toggleTaskForm();
+        taskManager.render();
+        taskManager.save();
+    }
+};
+
+
+// Delete task and close form for click on top-right X
+function closeFormX () {
+    let result = window.confirm("Are you sure you want to close and clear form?")
+    if (result) {
+        clearForm();
+        toggleTaskForm();
+    }
+};
+      
+       
+    
